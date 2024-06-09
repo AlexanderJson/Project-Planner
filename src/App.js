f5dbb95db45f5b2105144d185d2e89503b286bd6
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-06-09 18:12:41 +0200

    added Router to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { Suspense, lazy, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+const LazyLogin = lazy(() => import('./comps/AuthPage/Login'));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+     <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<LazyLogin />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+      </Suspense>
+     </Router>
     </div>
   );
 }
